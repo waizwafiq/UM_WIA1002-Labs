72bb10103a7186f2e792328236b77eb759f0f222
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-18 02:17:21 +0800

    Answered Q3

--- src/week10/Q3.java
+++ src/week10/Q3.java
@@ -1,4 +1,14 @@
 package week10;
 
 public class Q3 {
+    public static void main(String[] args) {
+        System.out.println(exponent(95, 7));
+    }
+
+    public static long exponent(int x, int y) {
+        if (y == 0)
+            return 1;
+
+        return x * exponent(x, y - 1);
+    }
 }
