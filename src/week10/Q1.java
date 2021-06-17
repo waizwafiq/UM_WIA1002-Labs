72fd6fa527cb8399412540807b18d14b939d56fb
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-17 19:18:25 +0800

    Answered Q1

--- src/week10/Q1.java
+++ src/week10/Q1.java
@@ -1,5 +1,21 @@
 package week10;
 
 public class Q1 {
+    public static void main(String[] args) {
+        String str = "Astronaut";
+        System.out.println(substituteAI(str, str.length()));
+    }
 
+    public static String substituteAI(String str, int i) {
+        if (i == 0)
+            return str;
+
+        if (str.charAt(i - 1) == 'a') {
+            char[] chars = str.toCharArray();
+            chars[i - 1] = 'i';
+            str = String.valueOf(chars);
+        }
+
+        return substituteAI(str, i - 1);
+    }
 }
