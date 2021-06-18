e4fc7905d029ef279f03ab2e27dcc9cb8fbed1d8
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-18 21:02:54 +0800

    Implemented BST methods

--- src/week13/BST.java
+++ src/week13/BST.java
@@ -10,4 +10,117 @@ public class BST<E extends Comparable<E>> {
             left = right = null;
         }
     }
+
+    private TreeNode<E> root;
+
+    public BST() {
+        root = null;
+    }
+
+    public BST(E e) {
+        assert false;
+        root.element = e;
+    }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    public TreeNode<E> add(TreeNode<E> current, E e) {
+        if (current == null)
+            current = new TreeNode<>(e);
+        else if (e.compareTo(current.element) <= 0)
+            current.left = add(current.left, e);
+        else
+            current.right = add(current.right, e);
+
+        return current;
+    }
+
+    private boolean found;
+
+    public boolean remove(E e) {
+        root = remove(root, e);
+        return found;
+    }
+
+    public TreeNode<E> remove(TreeNode<E> current, E e) {
+        if (current == null)
+            found = false;
+        else if (e.compareTo(current.element) < 0)
+            current.left = remove(current.left, e);
+        else if (e.compareTo(current.element) > 0)
+            current.right = remove(current.right, e);
+        else {
+            current = removeNode(current);
+            found = true;
+        }
+
+        return current;
+    }
+
+    private TreeNode<E> removeNode(TreeNode<E> node) {
+        if (node.left == null)
+            return node.right;
+        else if (node.right == null)
+            return node.left;
+        else {
+            E e = getPredecessor(node.left);
+            node.element = e;
+            node.left = remove(node.left, e);
+            return node;
+        }
+    }
+
+    public E getPredecessor(TreeNode<E> current) {
+        while (current.right != null)
+            current = current.right;
+
+        return current.element;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int getSize() {
+        return getSize(root);
+    }
+
+    public int getSize(TreeNode<E> current) {
+        if (current == null)
+            return 0;
+        else
+            return getSize(current.left) + getSize(current.right) + 1;
+    }
+
+    public boolean contains(E e) {
+        return find(root, e);
+    }
+
+    public boolean find(TreeNode<E> current, E e) {
+        if (current == null)
+            return false;
+        else if (e.compareTo(current.element) < 0)
+            return find(current.left, e); // go to the left child
+        else if (e.compareTo(current.element) > 0)
+            return find(current.right, e); // go to the right child
+        else
+            return true;
+    }
+
+    public E getElement(E e) {
+        return getElement(root, e);
+    }
+
+    public E getElement(TreeNode<E> current, E e) {
+        if (current == null)
+            return null;
+        else if (e.compareTo(current.element) < 0)
+            return getElement(current.left, e); // go to the left child
+        else if (e.compareTo(current.element) > 0)
+            return getElement(current.right, e); // go to the right child
+        else
+            return current.element;
+    }
 }
