3239f93cb581653daaed1522c17ab7ffbcfd1df4
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-18 21:02:12 +0800

    Implemented TreeNode class

--- src/week13/BST.java
+++ src/week13/BST.java
@@ -1,5 +1,13 @@
 package week13;
 
 public class BST<E extends Comparable<E>> {
+    private static class TreeNode<T> {
+        private TreeNode<T> left, right;
+        private T element;
 
+        public TreeNode(T element) {
+            this.element = element;
+            left = right = null;
+        }
+    }
 }
