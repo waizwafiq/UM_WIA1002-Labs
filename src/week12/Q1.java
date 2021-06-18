843368b44f23aad1a4db4a763fc7847daf4d9876
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-18 11:54:37 +0800

    Added selectionSortLargest()

--- src/week12/Q1.java
+++ src/week12/Q1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Q1 {
     public static void main(String[] args) {
-        int[] t = new int[] {3,2,5,1,6};
+        int[] t = new int[]{3, 2, 5, 1, 6};
         selectionSortSmallest(t);
         System.out.println(Arrays.toString(t));
     }
@@ -22,5 +22,16 @@ public class Q1 {
         }
     }
 
+    public static void selectionSortLargest(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] > arr[idx])
+                    idx = j;    //searching for lowest index
 
+            int smallerNumber = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
 }
