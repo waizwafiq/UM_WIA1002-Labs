c00b45eabe1c9adb009a396a4109c7795c9f4a7c
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-18 12:07:08 +0800

    Made minor changes

--- src/week12/Q4.java
+++ src/week12/Q4.java
@@ -11,13 +11,14 @@ public class Q4 {
 
     public static void insertionSort(int[] arr) {
         int n = arr.length;
-        for (int j = 1; j < n; j++) {
-            int key = arr[j], i = j - 1;
-            while ((i >= 0) && (arr[i] > key)) {
-                arr[i + 1] = arr[i];
-                i--;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i], j = i - 1;
+
+            while ((j >= 0) && (arr[j] > key)) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            arr[i + 1] = key;
+            arr[j + 1] = key;
         }
     }
 }
