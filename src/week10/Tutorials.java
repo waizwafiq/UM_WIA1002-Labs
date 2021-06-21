da1c7bc588b16b7a326333cfefd4a771caf8dfd4
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-21 15:25:49 +0800

    Solved Q3 Tutorial

--- src/week10/Tutorials.java
+++ src/week10/Tutorials.java
@@ -1,5 +1,15 @@
 package week10;
 
-public class Tutorials {
+class T_Q3 {
+    public static void main(String[] args) {
+        System.out.println(reverse("abcdef"));
+    }
 
+    public static String reverse(String str)
+    {
+        if (str.isEmpty())
+            return str;
+
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
 }
