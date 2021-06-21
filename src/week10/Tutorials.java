0a1e0ffce0f2fec6561924ff0af83cc3e666f2f6
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-21 15:37:47 +0800

    Solved Q4

--- src/week10/Tutorials.java
+++ src/week10/Tutorials.java
@@ -1,8 +1,7 @@
 package week10;
 
 class T_Q3 {
-    public static String reverse(String str)
-    {
+    public static String reverse(String str) {
         if (str.isEmpty())
             return str;
 
@@ -11,5 +10,15 @@ class T_Q3 {
 }
 
 class T_Q4 {
+    public static void printDigit(int integer) {
+        if (integer / 10 == 0) {
+            //if there's one digit left, just print the integer and stop the recursive function (base case)
+            System.out.println(integer);
+            return;
+        }
 
+        String str = String.valueOf(integer);  //change integer to string
+        System.out.print(str.charAt(0) + " "); //print the first digit + " "
+        printDigit(Integer.parseInt(str.substring(1))); //call the function recursively and truncate the first digit
+    }
 }
