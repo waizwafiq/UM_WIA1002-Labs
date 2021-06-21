0ffdd284bd1d2fd81b026cbb8ec55a0b99dbdfbc
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-21 15:26:25 +0800

    Created Q4 Tutorial question

--- src/week10/Tutorials.java
+++ src/week10/Tutorials.java
@@ -1,10 +1,6 @@
 package week10;
 
 class T_Q3 {
-    public static void main(String[] args) {
-        System.out.println(reverse("abcdef"));
-    }
-
     public static String reverse(String str)
     {
         if (str.isEmpty())
@@ -12,4 +8,8 @@ class T_Q3 {
 
         return reverse(str.substring(1)) + str.charAt(0);
     }
+}
+
+class T_Q4 {
+
 }
