cb447c91dc05b4ab4ae84d74842a43f3df07dc85
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-24 19:17:44 +0800

    Completed Q1 (c)

--- src/past_2019/Q1c.java
+++ src/past_2019/Q1c.java
@@ -2,6 +2,38 @@ package past_2019;
 
 public class Q1c {
     public static void main(String[] args) {
+        MyLinkedList<Integer> list1 = new MyLinkedList<>();
+        MyLinkedList<Integer> list2 = new MyLinkedList<>();
+        MyLinkedList<Integer> newList;
 
+        list1.addFirst(51);
+        list1.addFirst(41);
+        list1.addFirst(38);
+        list1.addFirst(10);
+        list1.addFirst(2);
+
+        list2.addLast(5);
+        list2.addLast(12);
+        list2.addLast(18);
+        list2.addLast(21);
+        list2.addLast(35);
+        list2.addLast(41);
+        list2.addLast(56);
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        newList = combine(list1, list2);
+
+        System.out.println("The newList is:\n" + newList);
+    }
+
+    public static MyLinkedList<Integer> combine(MyLinkedList<Integer> l1, MyLinkedList<Integer> l2) {
+        MyLinkedList<Integer> outList = new MyLinkedList<>(l1);
+        outList.getTail().setNext(l2.getHead());
+        outList.setSize(l1.getSize() + l2.getSize());
+        outList.sort();
+
+        return outList;
     }
 }
