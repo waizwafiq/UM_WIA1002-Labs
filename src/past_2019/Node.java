e946488c97f9d2c31d84cbb5fe34a874fe71d520
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-24 18:28:44 +0800

    Created node class 1(a)

---
+++ src/past_2019/Node.java
@@ -0,0 +1,26 @@
+package past_2019;
+
+public class Node<T> {
+    private T element;
+    private Node<T> next;
+
+    public Node(T element) {
+        this.element = element;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
